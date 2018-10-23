fceb762b6dca4b312ceef9257674c6ba1500a8e2
Ravern Koh <ravernkoh@gmail.com>
2018-10-23 10:20:15 +0800

    Setup structure (#1)
    * Add .gitignore
    
    * Add data/ to .gitignore
    
    * Create Python module and sample main.py
    
    * Create parser with subcommands

---
+++ main.py
@@ -0,0 +1,22 @@
+from argparse import ArgumentParser
+import coco_tools
+
+
+def main():
+    parser = ArgumentParser(description="Useful operations for COCO datasets")
+    subparsers = parser.add_subparsers(help="Possible operations")
+
+    split_parser = subparsers.add_parser("split", help="Splits a dataset")
+    split_parser.add_argument("dataset", help="The dataset to split")
+
+    merge_parser = subparsers.add_parser("merge", help="Merges datasets")
+    merge_parser.add_argument("datasets", nargs="+",
+                              help="The datasets to merge")
+
+    args = parser.parse_args()
+
+    print(args)
+
+
+if __name__ == "__main__":
+    main()
