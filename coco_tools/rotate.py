c4c9445724e7e983fcba08a8e91ef742c40197f7
Ravern Koh <ravernkoh@gmail.com>
2018-11-04 14:46:15 +0800

    Add json dump of output

--- coco_tools/rotate.py
+++ coco_tools/rotate.py
@@ -1,8 +1,11 @@
+import json
 import numpy as np
+from pathlib import Path
+from coco_tools.error import COCOToolsError
 
 
 def rotate(dataset_path, degrees):
-    """Rotates the dataset clockwise by the given number of degrees.
+    """Rotates the dataset counter-clockwise by the given number of degrees.
 
     This assumes that the annotations in the dataset are all of the "Object
     Detection" kind. Also assumed that `iscrowd` is 0, which means that polygon
@@ -12,7 +15,28 @@ def rotate(dataset_path, degrees):
     rotating each point in `segmentation` and `bbox` according to it.
     """
 
+    dataset_path = Path(dataset_path)
+
+    raw_data = None
+    try:
+        with open(str(dataset_path), "r") as dataset_file:
+            raw_data = json.load(dataset_file)
+    except FileNotFoundError:
+        raise COCOToolsError(f"file \"{dataset_path}\" not found")
+
+    with open(__derive_path(dataset_path), "w") as output_file:
+        json.dump(raw_data, output_file)
+
     raise NotImplementedError()
+
+
+def __derive_path(dataset_path):
+    """Derives the output path given `dataset_path`.
+    """
+
+    output_filename = Path(f"{str(dataset_path.stem)}_rotated.json")
+    output_path = dataset_path.parent / output_filename
+    return output_path
 
 
 def __rotate_point(point, origin, degrees):
